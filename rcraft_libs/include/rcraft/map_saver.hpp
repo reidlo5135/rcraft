bc54771ea466a359b31dff65bdc2aa45cae696f6
reidlo5135 <reidlo4215@gmail.com>
2025-08-19 16:07:31 +0900

    refactor : modify driving map conversion logics

---
+++ rcraft_libs/include/rcraft/map_saver.hpp
@@ -0,0 +1,27 @@
+#ifndef RCRAFT_MAP_SAVER_HPP
+#define RCRAFT_MAP_SAVER_HPP
+
+#include <memory>
+#include <iostream>
+#include <filesystem>
+#include <opencv2/opencv.hpp>
+
+namespace fs = std::filesystem;
+
+namespace rcraft::map
+{
+    class MapSaver final
+    {
+    public:
+        explicit MapSaver();
+        virtual ~MapSaver();
+
+        void save(const cv::Mat &map, const std::string &path);
+
+    public:
+        using SharedPtr = std::shared_ptr<MapSaver>;
+        using UniquePtr = std::shared_ptr<MapSaver>;
+    };
+}
+
+#endif // RCRAFT_MAP_SAVER_HPP
